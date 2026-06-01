0332649ed1a6a1a842c0825a939ab88138dcacf6
LitheFulminare <vitor.rossafa@pucpr.edu.br>
2026-06-01 20:57:04 -0300

    progresso mudar volume de cada bus de áudio
    novos sliders e funções criadas, progresso tá pela metade

--- Scripts/Mechanics/Scenes/settingsMenu_manager.gd
+++ Scripts/Mechanics/Scenes/settingsMenu_manager.gd
@@ -8,15 +8,31 @@ var current_parent: Node
 ## Variáveis dos nodes na cena do menu de opções
 @onready var animation_player = $Transition_FX
 @onready var background = $Settings_BG
+
+# usado na hora de mudar o volume de cada bus de áudio
+var music_bus_index: int
+var sfx_bus_index: int
+
 func _ready() -> void:
 	current_parent = get_parent()
-## Essa função pega o valor que o usuário colocou de volume e muda as configurações do jogo
-func _on_volume_changed(value: float) -> void:
-	AudioServer.set_bus_volume_db(0, linear_to_db(value))
+	music_bus_index = AudioServer.get_bus_index("Music")
+	sfx_bus_index = AudioServer.get_bus_index("SFX")
+	
+## Altera o volume do bus de música
+func _on_music_volume_changed(value: float) -> void:
+	AudioServer.set_bus_volume_db(music_bus_index, linear_to_db(value))
 
-## Essa função verifica se a caixa de mutar o jogo foi verificada
-func _on_mute_toggled(toggled_on: bool) -> void:
-	AudioServer.set_bus_mute(0, toggled_on)
+## Muta e desmuta o bus de música
+func _on_music_mute_toggled(toggled_on: bool) -> void:
+	AudioServer.set_bus_mute(music_bus_index, toggled_on)
+	
+## Altera o volume do bus de SFX
+func _on_sfx_volume_changed(value: float) -> void:
+	AudioServer.set_bus_volume_db(sfx_bus_index, linear_to_db(value))
+
+## Muta e desmuta o bus de SFX
+func _on_sfx_mute_toggled(toggled_on: bool) -> void:
+	AudioServer.set_bus_mute(sfx_bus_index, toggled_on)
 
 ## Essa função verifica qual opção de tela foi selecionada
 func _on_screen_options_selected(index: int) -> void:
